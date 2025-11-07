5a98edcd8ccd48ff342ed0fe74bc336d38c205f6
aver1ch <vavericherry3@gmail.com>
2025-11-07 04:48:35 +0300

    modified loginHandler func

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"backend/internal/handlers"
+	"log/slog"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	slog.Info("Server starts")
+
+	distDir := "../frontend/dist"
+	fs := http.FileServer(http.Dir(distDir))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := filepath.Join(distDir, r.URL.Path)
+
+		_, err := os.Stat(path)
+
+		if os.IsNotExist(err) {
+			http.ServeFile(w, r, filepath.Join(distDir, "index.html"))
+			return
+		}
+
+		fs.ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("/auth", handlers.LoginHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ backend/internal/handlers/loginHandler.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
+
+type credentials struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	slog.Debug("Check is request is POST in LoginHandler function")
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var credentials credentials
+
+	slog.Debug("Pull login and password from request in LoginHandler function")
+	if err := json.NewDecoder(r.Body).Decode(&credentials); err != nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
+
+	slog.Debug("Check the login and password")
+	if credentials.Login == "admin" && credentials.Password == "1234" {
+		slog.Debug("Login and password is valid")
+
+		slog.Debug("Cooking response")
+		response := map[string]string{
+			"token": "vovan",
+		}
+
+		slog.Debug("Sending response")
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+
+	http.Error(w, "invalid credentials", http.StatusUnauthorized)
+}
